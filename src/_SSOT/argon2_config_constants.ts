06d75db03055dc9b85da161dd97f55757a85a6fe
Jacob Stark <90098681+nagaistark@users.noreply.github.com>
2026-03-22 11:36:50 +0500

    Argon2Regex added

--- src/_SSOT/argon2_config_constants.ts
+++ src/_SSOT/argon2_config_constants.ts
@@ -10,3 +10,6 @@ export const ARGON2_CONFIG: Options & { type: 2 } = {
    hashLength: 32,
    secret: myEnv.argon2Secret,
 };
+
+export const argon2Regex =
+   /^\$argon2id\$v=19\$m=65536,t=3,p=4\$[A-Za-z0-9+/]{22}\$[A-Za-z0-9+/]{43}$/;
